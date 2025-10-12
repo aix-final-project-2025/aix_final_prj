e0a19b34c70ce42d03d478db4d00a5cfa77874a5
nakrlove <nakrlove@gmail.com>
2025-10-12 14:14:22 +0900

    시연용

--- aix_final_prj/service/news.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-
-class NewsPage(View):
-    template_name = "news.html"
-
-    def get(self, request):
-        context = {"title": "Dev1", "message": "클래스 기반 뷰"}
-        return render(request, self.template_name, context)
