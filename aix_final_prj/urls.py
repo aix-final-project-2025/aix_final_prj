88bd372c0351e718169e696013cf8b545ef30e44
nakrlove <nakrlove@gmail.com>
2025-10-12 14:14:22 +0900

    시연용

--- aix_final_prj/urls.py
+++ aix_final_prj/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path("dev/", include("aix_final_prj.dev.urls1")),  # 개발자 1
     path("dev/", include("aix_final_prj.dev.urls2")),  # 개발자 2
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
